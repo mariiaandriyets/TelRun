bf681377dd2399e096dfa703ec9db0fea0d581d4
mariiaandriyets <129598311+mariiaandriyets@users.noreply.github.com>
2023-08-08 18:21:23 +0200

    Add files via upload

---
+++ homework9/index.js
@@ -0,0 +1,45 @@
+
+// }
+//задание 1
+// const arr = [1,3,5,7,9]
+// const result = []
+// for(let i = 0; i < arr.length; i++) {
+//     let it =arr[i]
+//     result.push(it)
+// }
+// console.log(result)
+
+//задание 2
+// const arr1 = [];
+// for(let i = 20; i<=55; i++) {
+//    arr1.push(i)
+// }
+// console.log(arr1)
+
+
+  
+  //задание 3
+// const numbers = [3, 5, 11, 2, 8, 1, 6]
+// console.log(numbers)
+
+
+//задание 4-5
+
+    //  const numbers = [3, 5, 11, 2, 8, 1, 6]
+    //  const numbers_squared = []
+    //  for (let i = 0; i < numbers.length; i++) {
+    //      let it = numbers[i] ** 2
+    //     numbers_squared.push(it)
+    //   }
+    //  const last_elem = numbers_squared[6]
+    //  console.log(last_elem)
+    //  console.log(numbers_squared)
+
+    //задание 6
+    // const user = {
+    //     first_name: 'Mary',
+    //     last_name: 'A',
+    //     age: 18,
+    //     salary: 11000,
+    // }
+    // console.log(user.first_name)
